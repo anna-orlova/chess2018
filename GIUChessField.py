81dbb54c8f1a679fc2d2f5abdbc12b5353b99105
Anna Orlova <anna@orlova.ru>
2018-08-13 20:01:15 +0200

    Changed class GUIChessField , add QLAbel instead QPushButton

--- GIUChessField.py
+++ GIUChessField.py
@@ -1,10 +1,23 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from Figure import *
 
-class GUIChessField(QPushButton):
+class GUIChessField(QLabel):
     def __init__(self, symbol, parent=None):
         super(GUIChessField, self).__init__(parent)
         self.__symbol = symbol
+
+    def setFigure(self, figure):
+        self.__figure = figure
+        if figure is None:
+            self.setText(" ")
+        else:
+            color = str(figure.GetColor())
+            symbol = str(figure.GetSymbol())
+            self.setText(color + symbol)
+
+    def mousePressEvent(self, QMouseEvent):
+        self.emit(SIGNAL("clicked()"))
 
     def GetSymbol(self):
         return self.__symbol
