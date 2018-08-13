d97ede3102799e0306537128e0b44bd1b373f9c2
Anna Orlova <anna@orlova.ru>
2018-08-13 22:36:45 +0200

    Print the svg names of the figures in console.

--- GIUChessField.py
+++ GIUChessField.py
@@ -3,9 +3,19 @@ from PyQt4.QtGui import *
 from Figure import *
 
 class GUIChessField(QLabel):
+
+    FPictures = "C:\\Users\\Anna\\Documents\\Chess2018\\Pictures\\"
+    Files_svg = {"WP": FPictures + "W_Pawn.svg", "WT": FPictures + "W_Tor.svg",
+                 "WN": FPictures + "W_Knight.svg", "WB": FPictures + "W_Bishop.svg",
+                 "WQ": FPictures + "W_Queen.svg", "WK": FPictures + "W_King.svg",
+                 "BP": FPictures + "B_Pawn.svg", "BT": FPictures + "B_Tor.svg",
+                 "BN": FPictures + "B_Knight.svg", "BB": FPictures + "B_Bishop.svg",
+                 "BQ": FPictures + "B_Queen.svg", "BK": FPictures + "B_King.svg"}
+
     def __init__(self, symbol, parent=None):
         super(GUIChessField, self).__init__(parent)
         self.__symbol = symbol
+
 
     def setFigure(self, figure):
         self.__figure = figure
@@ -14,7 +24,9 @@ class GUIChessField(QLabel):
         else:
             color = str(figure.GetColor())
             sym = str(figure.GetSymbol())
+            index = color + sym
             self.setText(color + sym)
+            print(self.Files_svg[index])
 
     def mousePressEvent(self, QMouseEvent):
         self.emit(SIGNAL("clicked()"))
